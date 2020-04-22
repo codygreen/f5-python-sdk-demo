641bb348f02f0bb90f754491b943ba4e793eb98e
Cody Green <cody@codygreen.com>
2020-04-22 03:25:26 +0000

    f5 sdk config update

--- README.md
+++ README.md
@@ -5,15 +5,14 @@ Demo of how to use the F5 Python SDK to configure a BIG-IP
 This demo requires:
 - Python3
 - [F5 SDK](https://clouddocs.f5.com/sdk/f5-sdk-python/quickstart/) to communicate with the BIG-IP
-- [keyring](https://pypi.org/project/keyring/) to store the BIG-IP password
 
 # Installation
 ```bash
-sudo pip3 install keyring
 sudo pip3 install f5-sdk-python
 ```
 
 # Running the Demo
 ```bash
+export bigipPassword="yourpassword"
 python3 bigip.py data.yml
 ```
--- bigip.py
+++ bigip.py
@@ -7,7 +7,7 @@ import jsonschema
 import requests
 from jinja2 import Environment, FileSystemLoader
 from f5sdk.bigip import ManagementClient
-from f5sdk.bigip.extension import DOClient
+from f5sdk.bigip.extension import AS3Client, DOClient
 from f5sdk.logger import Logger
 
 LOGGER = Logger(__name__).get_logger()
@@ -17,7 +17,6 @@ def create_declaration(data, atc_type):
     """ 
     create automation toolchain declaration 
     """
-    print(data)
     file_loader = FileSystemLoader('templates')
     env = Environment(loader=file_loader)
 
@@ -34,6 +33,36 @@ def create_declaration(data, atc_type):
     f.write(output)
     f.close()
     return True
+
+def update_config(data, atc_type):
+    """
+    Update AS3 or DO configuration
+    """
+    # create management client
+    mgmt_client = ManagementClient(
+        data['f5SdkHost'],
+        user = data['f5SdkUsername'],
+        password = os.getenv('bigipPassword')
+    )
+
+    # create extension client
+    client = AS3Client(mgmt_client) if atc_type == "as3" else DOClient(mgmt_client)
+
+    # Get installed package version info
+    version_info = client.package.is_installed()
+    LOGGER.info(version_info['installed'])
+    LOGGER.info(version_info['installed_version'])
+    LOGGER.info(version_info['latest_version'])
+
+    # Install package
+    if not version_info['installed']:
+        client.package.install()
+
+    # ensure service is available
+    client.service.is_available()
+
+    # configure service
+    return client.service.create(config_file='./files/{}.{}.json'.format(data['hostname'].split('.')[0], atc_type))
 
 if __name__ == '__main__':
     # Read BIG-IP data from YAML file or environment variables
@@ -58,3 +87,5 @@ if __name__ == '__main__':
     for bigip in bigip_data['bigips']:
         LOGGER.info(create_declaration({ **bigip,  **bigip_data['dataCenters'][bigip['dataCenter']] },"do"))
         LOGGER.info(create_declaration(bigip, "as3"))
+        LOGGER.info(update_config(bigip, "do"))
+        LOGGER.info(update_config(bigip, "as3"))
